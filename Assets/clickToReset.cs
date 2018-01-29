ac21ace167a09e1240adeae5aff263e7d2e159c8
Caleb J Caldwell <ccaldw2@uwo.ca>
2018-01-29 15:21:18 -0500

    Initial commit

---
+++ Assets/clickToReset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickToReset : MonoBehaviour {
+
+    Vector3 originalPos;
+    Quaternion originalRot;
+
+    void Start()
+    {
+        originalPos = gameObject.transform.position;
+        originalRot = gameObject.transform.rotation;
+    }
+
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1)) { 
+            gameObject.transform.position = originalPos;
+            gameObject.transform.rotation = originalRot;
+    }
+    }
+}
